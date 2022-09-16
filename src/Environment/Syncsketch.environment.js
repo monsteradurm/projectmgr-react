53b149d9dc201688a00e79cefefdf0ac47460752
monsteradurm <acranch@liquidanimation.com>
2022-09-16 16:10:01 +1000

    fallback reverse proxy

--- src/Environment/Syncsketch.environment.js
+++ src/Environment/Syncsketch.environment.js
@@ -6,26 +6,26 @@ export const SyncsketchConfig = {
     url: 'https://syncsketch.com/api/v2/',
     account: '116681',
 }
-const apiv1 = '/reverseProxy/syncsketch.com/api/v1'
-const apiv2 = '/reverseProxy/syncsketch.com/api/v2'
-const upload =  '/reverseProxy/syncsketch.com/items/uploadToReview'
+const apiv1 = 'syncsketch.com/api/v1'
+const apiv2 = 'syncsketch.com/api/v2'
+const upload =  'syncsketch.com/items/uploadToReview'
 
 export const SyncsketchPosts = {
-    RenameItem: (id) => `${apiv1}/item/${id}/`,
+    RenameItem: (id) => `${ReverseProxy}${apiv1}/item/${id}/`,
     UploadFile: (reviewId) => `/syncsketch-upload/${reviewId}/?noConvertFlag=0`,
-    UpdateItemSort: (reviewId) => `${apiv2}/review/${reviewId}/sort_items/`,
+    UpdateItemSort: (reviewId) => `${ReverseProxy}${apiv2}/review/${reviewId}/sort_items/`,
     
     CreateReview: `${apiv1}/review/`,
 }
 
 export const SyncsketchQueries = {
-    AllUsers: `${apiv2}/account/${SyncsketchConfig.account}/`,
-    ActiveProjects: `${apiv1}/project/?active=1&is_archived=0&account__active=1`,
+    AllUsers: `${ReverseProxy}${apiv2}/account/${SyncsketchConfig.account}/`,
+    ActiveProjects: `${ReverseProxy}${apiv1}/project/?active=1&is_archived=0&account__active=1`,
 
-    ReviewsByName: (name) => `${apiv1}/review/?name__istartswith=${name}&active=1`,
-    ItemById: (id) => `${apiv1}/item/${id}/`,
-    ThumbnailById: (id) => `${apiv1}/item/${id}/?fields=id,thumbnail_url,name`,
-    AllFeedback: (itemId) => `${apiv1}/frame/?item__id=${itemId}`,
-    ItemsByReview: (reviewId) => `${apiv1}/item/?reviews__id=${reviewId}&active=1`,
-    ReviewsByProjectId: (id) => `${apiv1}/review/?project__id=${id}&active=1&fields=id,name,description,review_url,group`
+    ReviewsByName: (name) => `${ReverseProxy}${apiv1}/review/?name__istartswith=${name}&active=1`,
+    ItemById: (id) => `${ReverseProxy}${apiv1}/item/${id}/`,
+    ThumbnailById: (id) => `${ReverseProxy}${apiv1}/item/${id}/?fields=id,thumbnail_url,name`,
+    AllFeedback: (itemId) => `${ReverseProxy}${apiv1}/frame/?item__id=${itemId}`,
+    ItemsByReview: (reviewId) => `${ReverseProxy}${apiv1}/item/?reviews__id=${reviewId}&active=1`,
+    ReviewsByProjectId: (id) => `${ReverseProxy}${apiv1}/review/?project__id=${id}&active=1&fields=id,name,description,review_url,group`
 }
