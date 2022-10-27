2f5e40bcd64863dfa9ece26948aba7bc38a1ae42
monsteradurm <acranch@liquidanimation.com>
2022-10-28 09:32:34 +1000

    timesheets / logs

--- src/Components/Timesheet/Timesheet.component.jsx
+++ src/Components/Timesheet/Timesheet.component.jsx
@@ -16,6 +16,7 @@ import { SetTimesheetRange, useTimesheetRange, ThisWeek } from "./Timesheet.cont
 import { Loading } from "../General/Loading";
 import { SUSPENSE } from "@react-rxjs/core";
 import { TimesheetLogs } from "./Timesheet.Logs";
+import { toggleStatusFilter } from "../Project/Overview.filters";
 
 const DateTemplate = (sheet) => {
     const date = sheet?.date;
@@ -35,25 +36,6 @@ const RibbonTemplate = (sheet) => {
     return <div style={{background, width: '100%', minWidth:'100%', maxWidth: '100%', height:'100%'}}></div>
 }
 
-const ProjectTags = ({projects}) => {
-    const id = useId();
-
-    if (!projects) return <></>
-
-    return <Stack direction="horizontal" gap={1}>
-    {
-    projects.map(p => <div className="log-project-tag" key={id + "_" + p}>{p}</div>)
-    }
-    </Stack>
-}
-
-const ProjectsTemplate = (sheet) => {
-    let projects = sheet?.logs?.map(l => l.ProjectId).filter(p => !!p) || [];
-    if (projects.length < 1) return <></>
-
-    projects = _.uniq(projects).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
-    return <ProjectTags projects={projects} />
-}
 
 const TasksTemplate = (sheet) => {
     if (!sheet?.logs) return <></>
@@ -71,20 +53,6 @@ const TomorrowTemplate = (log) => {
     return log?.tomorrow && <FontAwesomeIcon icon={faCircleCheck} className="log-tick" />;
 }
 
-const FeedbackDepartmentsTemplate = (sheet) => {
-    let departments = sheet?.logs?.map(l => l.FeedbackDepartment).filter(d => !!d) || [];
-    if (departments.length < 1) return <></>
-
-    departments = _.uniq(departments).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
-    return <ProjectTags projects={departments} />
-}
-const DepartmentsTemplate = (sheet) => {
-    let departments = sheet?.logs?.map(l => l.Department).filter(d => !!d) || [];
-    if (departments.length < 1) return <></>
-
-    departments = _.uniq(departments).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
-    return <ProjectTags projects={departments} />
-}
 
 const SubmittedTemplate = (log) => {
     return log?.submitted ? <FontAwesomeIcon icon={faCircleCheck} className="log-tick" /> 
@@ -110,30 +78,118 @@ export const TimesheetComponent = ({}) => {
     const [taskCount, setTaskCount] = useState(null);
     const [reviewCount, setReviewCount] = useState(null);
     const [projectCount, setProjectCount] = useState(null);
-
+    const [projectFilter, setProjectFilter] = useState(null);
+    const [departmentFilter, setDepartmentFilter] = useState(null);
+    const [feedbackFilter, setFeedbackFilter] = useState(null);
+    const [approversFilter, setApproversFilter] = useState(null)
     const [expandedRows, setExpandedRows] = useState([]); 
+
+    const [forcedRefresh, setForcedRefresh] = useState(0);
 
     SetNavigationHandler(useNavigate());
     
     useEffect(() => {
+        let dep = searchParams.get("Department");
+        if (!dep?.length)
+            dep = null;
+        let fb = searchParams.get("FeedbackDepartment");
+        if (!fb?.length)
+            fb = null;
+
+        let app = searchParams.get("Approvers");
+        if (!app?.length)
+            app = null;
+
+        let proj = searchParams.get("Project");
+        if (!proj?.length)
+            proj = null;
+
+        let searchChanged = false;
+
+        if (proj !== projectFilter)
+            setProjectFilter(proj);
+        
+        if(proj === null && searchParams.has('Project')) {
+            searchParams.delete('Project');
+            searchChanged = true;
+        }
+
+        if (dep !== departmentFilter)
+            setDepartmentFilter(dep);
+
+        if(dep === null && searchParams.has('Department')) {
+            searchParams.delete('Department');
+            searchChanged = true;
+        }
+
+        if (fb !== feedbackFilter)
+            setFeedbackFilter(fb);
+
+        if(!fb === null && searchParams.has('Feedback')) {
+            searchParams.delete('Feedback');
+            searchChanged = true;
+        }
+
+        if (app !== approversFilter)
+            setApproversFilter(app);
+
+        if(app === null && searchParams.has('Approvers')) {
+            searchParams.delete('Approvers');
+            searchChanged = true;
+        }
+
+        if (searchChanged)
+            setSearchParams(searchParams);
+        
+        setForcedRefresh(forcedRefresh  + 1);
+    }, [searchParams])
+
+    useEffect(() => {
+        
         if (Timesheets === SUSPENSE)
             return;
 
         let result = [...Timesheets];
+        if (projectFilter?.length || feedbackFilter?.length || departmentFilter?.length || approversFilter?.length) {
+            result = result.filter(r => r?.logs?.length);
+            console.log("filtering: ", {projectFilter, departmentFilter, feedbackFilter, approversFilter});
+            if (projectFilter?.length) {
+                result = result.filter(r => r?.logs?.filter(l => l.ProjectId.startsWith(projectFilter))?.length)
+            }
 
+            if (departmentFilter?.length) {
+                result = result.filter(r => r?.logs?.filter(l => l.Department === departmentFilter)?.length)
+            }
+
+            if (feedbackFilter?.length) {
+                result = result.filter(r => r?.logs?.filter(l => l.FeedbackDepartment === feedbackFilter)?.length)
+            }
+            if (approversFilter?.length) {
+                result = result.filter(r => {
+                    
+                    if (!r?.logs?.length) return false;
+                    
+                    const approved = r.approved;
+                    if (!approved || approved.length < 1)
+                        return false;
+
+                    return approved.map(a => a.replace(/\s/g, '')).indexOf(approversFilter) >= 0;
+                });
+            }
+        }
         setLogs(result);
 
         const sheets = result?.filter(l => !!l.artist);
         setSheetCount(sheets.length);
-        const entries = _.flatten(_.pluck(sheets, 'logs').filter(l => !!l && l.length));
+        const entries = _.flatten(_.pluck(logs, 'logs').filter(l => !!l && l.length));
 
         setHourCount(_.reduce(entries, (acc, e) => acc += e.hours || 0, 0));
         setTaskCount(_.uniq(_.pluck(entries, 'ItemId')).length);
         setProjectCount(_.uniq(_.pluck(entries, 'ProjectId')).length);
         setReviewCount(_.uniq(_.pluck(entries, 'ReviewId').filter(r => !!r)).length);
-        setReviewCount(_.reduce(entries, (acc, e) => acc += e.hours || 0, 0));
+        setHourCount(_.reduce(entries, (acc, e) => acc += e.hours || 0, 0));
 
-    }, [Timesheets])
+    }, [Timesheets, feedbackFilter, departmentFilter, projectFilter, approversFilter, forcedRefresh])
 
     useEffect(() => {
         const range_str = JSON.stringify(Range);
@@ -157,9 +213,49 @@ export const TimesheetComponent = ({}) => {
         SetTitles(titles);
     }, [User])
 
+    
+    const TagComponent = ({tags, searchKey}) => {
+        const id = useId();
+
+        if (!tags) return <></>
+
+        return <Stack direction="horizontal" gap={1}>
+        {
+            tags.map(p => <div className="log-project-tag" key={id + "_" + p}
+                onClick={() => toggleStatusFilter(p, searchParams, setSearchParams, searchKey)}>
+                {p}
+            </div>)
+        }
+        </Stack>
+    }
+
+    const ProjectsTemplate = (sheet) => {
+        let projects = sheet?.logs?.map(l => l.ProjectId).filter(p => !!p) || [];
+        if (projects.length < 1) return <></>
+
+        projects = _.uniq(projects).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
+        return <TagComponent tags={projects} searchKey="Project"/>
+    }
+
+    const FeedbackDepartmentsTemplate = (sheet) => {
+        let departments = sheet?.logs?.map(l => l.FeedbackDepartment).filter(d => !!d) || [];
+        if (departments.length < 1) return <></>
+
+        departments = _.uniq(departments).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
+        return <TagComponent tags={departments} searchKey="Feedback"/>
+    }
+    const DepartmentsTemplate = (sheet) => {
+        let departments = sheet?.logs?.map(l => l.Department).filter(d => !!d) || [];
+        if (departments.length < 1) return <></>
+
+        departments = _.uniq(departments).map(p => p.indexOf('_') >= 0 ? p.split('_')[0] : p)
+        return <TagComponent tags={departments} searchKey="Department"/>
+    }
+
     if (Timesheets === SUSPENSE)
         return <Loading text="Retrieving Timesheets..." />
 
+    console.log("TIMESHEETS", Timesheets?.length)
     return (
         <>
         <TimeSheetFilterBar />
@@ -182,8 +278,41 @@ export const TimesheetComponent = ({}) => {
                 <Column header="Approved" body={ApprovedTemplate} className="log-approved" style={{width: 200, maxWidth: 200}}></Column>
                 <Column body={RibbonTemplate} style={{width: 15, maxWidth: 15}} className="log-ribbon" />
             </DataTable>
-            <Stack direction="horizontal" gap={1} style={{fontSize: 18, marginRight: 100, marginLeft: 100, marginTop: -30}}>
-                <div><span style={{fontWeight: 600}}>{sheetCount}</span> Timesheets...</div>
+            <Stack direction="horizontal" gap={1} className="pm-tag-filters"
+                style={{fontSize: 18, marginRight: 100, marginLeft: 100, marginTop: -30, zIndex: 100}}>
+                <div style={{marginRight: 20}}><span style={{fontWeight: 600}}>{sheetCount}</span> Timesheets...</div>
+                {
+                    projectFilter &&
+                    <div className="pm-tag" key={"ProjectFilter" + "_" + projectFilter}
+                        onClick={() => toggleStatusFilter(projectFilter, searchParams, setSearchParams, "Project")}>
+                        #{projectFilter}
+                    </div>
+
+                }
+                {
+                    departmentFilter &&
+                    <div className="pm-tag" key={"departmentFilter" + "_" + departmentFilter} 
+                        onClick={() => toggleStatusFilter(departmentFilter, searchParams, setSearchParams, "Department")}>
+                        #{departmentFilter}
+                    </div>
+
+                }
+                {
+                    feedbackFilter &&
+                    <div className="pm-tag" key={"feedbackFilter" + "_" + feedbackFilter} 
+                        onClick={() => toggleStatusFilter(feedbackFilter, searchParams, setSearchParams, "Feedback")}>
+                        #{feedbackFilter}
+                    </div>
+
+                }
+                {
+                    approversFilter &&
+                    <div className="pm-tag" key={"approversFilter" + "_" + approversFilter}
+                        onClick={() => toggleStatusFilter(approversFilter, searchParams, setSearchParams, "Approvers")}>
+                        #{approversFilter}
+                    </div>
+
+                }
                 <div className="mx-auto"></div>
                 <div style={{fontWeight: 600}}>{projectCount}</div>
                 <div>Projects, </div>
